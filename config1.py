2a05d52899b0461f87486415eda426a7d1448f7d
gfisherjr <60365739+gfisherjr@users.noreply.github.com>
2020-06-19 22:03:25 -0400

    final update?

---
+++ config1.py
@@ -0,0 +1,2 @@
+# Quandl API Gary 
+quandl_api_key = "SCsYW37j-Z5FNLh-Nfkp"
--- plots.js
+++ plots.js
@@ -1,25 +1,5 @@
-var apiKey = "SCsYW37j-Z5FNLh-Nfkp";
-
-/**
- * Helper function to select stock data
- * Returns an array of values
- * @param {array} rows
- * @param {integer} index
- * index 0 - Date
- * index 1 - Open
- * index 2 - High
- * index 3 - Low
- * index 4 - Close
- * index 5 - Volume
- */
-function unpack(rows, index) {
-  return rows.map(function(row) {
-    return row[index];
-  });
-}
-
 d3.csv("project_2_data_gary.csv").then(function(data) {
-  //var dates = unpack(data.Dates, 1)
+  
   var dates = []
   var sp500 = []
   var sp500corr = []
@@ -37,15 +17,11 @@ d3.csv("project_2_data_gary.csv").then(function(data) {
   data.forEach((d) => {
     filtersp500corr.push(d.filterSP500corr);
   })
-  
-  //console.log(data);
 
   console.log(dates);
   console.log(sp500);
   console.log(sp500corr);
   console.log(filtersp500corr);
-
-  //console.log(data[i].Date);
 })
 
 function updatePlotly() {
@@ -55,41 +31,24 @@ function updatePlotly() {
   var dataset = dropdownMenu.property("value");
   console.log(dataset);
 
-  // Initialize x and y arrays
-  var x = [];
-  var y = [];
-
   if (dataset === 'dataset1') {
-    buildPlot_gary();  
-    // x = [1, 2, 3, 4]
-    // y = [2, 3, 4, 5]
+    buildPlot_bruce();  
   }
 
   if (dataset === 'dataset2') {
-    buildPlot_bruce();
-    // x = [1, 2, 3, 4]
-    // y = [2, 3, 4, 5]
+    buildPlot_gary();
   }
 
   if (dataset === 'dataset3') {
     buildPlot_ben();
-    // x = [1, 2, 3, 4]
-    // y = [4, 5, 6, 7]
   }
-
-  // Note the extra brackets around 'x' and 'y'
-  Plotly.restyle("plot", "x", [x]);
-  Plotly.restyle("plot", "y", [y]);
 }
 
 // Call updatePlotly() when a change takes place to the DOM
 d3.selectAll("#selDataset").on("change", updatePlotly);
 
-
 function buildPlot_gary() {
-  //var url = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2017-01-01&end_date=2017-12-31&api_key=${apiKey}`;
-  //var url = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2017-01-01&end_date=2018-11-22&api_key=${apiKey}`;
-
+  
   d3.csv("project_2_data_gary.csv").then(function(data) {
     //var dates = unpack(data.Dates, 1)
     var dates = []
@@ -111,8 +70,6 @@ function buildPlot_gary() {
     data.forEach((d) => {low.push(d.Low);})
     data.forEach((d) => {close.push(d.Last);})
     data.forEach((d) => {returns.push(d.Return);})
-    
-    //console.log(data);
   
     console.log(dates);
     console.log(sp500);
@@ -123,8 +80,6 @@ function buildPlot_gary() {
     console.log(low);
     console.log(close);
     console.log(returns);
-  
-    //console.log(data[i].Date);
   
     var selectorOptions = {
       buttons: [{
@@ -167,26 +122,15 @@ function buildPlot_gary() {
           stepmode: 'backward',
           count: 20,
           label: '20y'
-    }, {
+      }, {
           step: 'all',
       }],
     };
 
-    // var trace1 = {
-    //   type: "scatter",
-    //   mode: "lines",
-    //   name: name,
-    //   x: dates,
-    //   y: sp500,
-    //   line: {
-    //     color: "red"
-    //   }
-    // };
-
     var trace2 = {
       type: "scatter",
       mode: "lines",
-      name: name,
+      name: "6-3MoSP500Corr",
       x: dates,
       y: filtersp500corr,
       yaxis: "y2",
@@ -198,6 +142,7 @@ function buildPlot_gary() {
     // Candlestick Trace
     var trace1 = {
       type: "candlestick",
+      name: "SP500",
       x: dates,
       high: high,
       low: low,
@@ -208,9 +153,9 @@ function buildPlot_gary() {
     var data = [trace1, trace2];
 
     var layout = {
-      title: `SP500 closing prices`,
+      title: `S&P 500 vs Correlation Between 6Mo-3Mo Treasury Yields and S&P 500`,
       // autosize: true,
-      // width: 500,
+      // width: "100%",
       height: 700,
       xaxis: {
         rangeselector: selectorOptions,
@@ -224,41 +169,31 @@ function buildPlot_gary() {
         side: "right"
       },
       yaxis2: {
-        title: 'yaxis2 title',
-        titlefont: {color: 'rgb(148, 103, 189)'},
-        tickfont: {color: 'rgb(148, 103, 189)'},
+        title: '6Mo - 3Mo & SP500 Correlation',
         overlaying: 'y',
         side: 'left'
       },
-      showlegend: true
+      showlegend: true,
+      legend: {
+        x: 1,
+        xanchor: 'right',
+        y: 1.1
+      }
     };
 
-    // Plotly - Making a Responsive Chart:
-    // https://plotly.com/javascript/configuration-options/#making-a-responsive-chart
     Plotly.newPlot("plot", data, layout, {responsive: true});
 
   });
 }
 
-buildPlot_gary();
-// getMonthlyData();
-
-
-
-// function init() {
-//   buildPlot_gary();
-// }
-
-
-
+// buildPlot_gary();
 
 //##############################################################
-d3.csv("jun98tojun20B.csv").then(function(data) {
+d3.csv("jun98tojun20.csv").then(function(data) {
   //var dates = unpack(data.Dates, 1)
   var dates = []
   var sp500 = []
   var signal = []
-  var cap_growth = []
   // var filtersp500corr = []
 
   data.forEach((d) => {
@@ -270,65 +205,28 @@ d3.csv("jun98tojun20B.csv").then(function(data) {
   data.forEach((d) => {
     signal.push(d.signal);
   })
-  data.forEach((d) => {
-    cap_growth.push(d.Capital);
+
+d3.csv("CGr_model_returns.csv").then(function(data){
+    var cap_growth = []
+
+  data.forEach((c) => {
+    cap_growth.push(c.Capital);
   })
-
-
- 
+})
+  
   console.log(cap_growth);
-  //console.log(data);
-
   console.log(dates);
   console.log(sp500);
   console.log(signal);
-  
-
-  //console.log(data[i].Date);
 })
 
-// function getMonthlyData() {
-//   // var queryUrl = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2017-01-01&end_date=2017-12-31&collapse=monthly&api_key=${apiKey}`;
-//   //var queryUrl = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2016-10-01&end_date=2017-10-01&collapse=monthly&api_key=${apiKey}`;
-//   d3.json(queryUrl).then(function(data) {
-//     var dates = unpack(data.dataset.data, 0);
-//     var openPrices = unpack(data.dataset.data, 1);
-//     var highPrices = unpack(data.dataset.data, 2);
-//     var lowPrices = unpack(data.dataset.data, 3);
-//     var closingPrices = unpack(data.dataset.data, 4);
-//     var volume = unpack(data.dataset.data, 5);
-//     console.log(openPrices);
-//     buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, volume);
-//   });
-// }
-
-// function buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, volume) {
-//   var table = d3.select("#summary-table");
-//   var tbody = table.select("tbody");
-//   var trow;
-//   console.log(dates);
-//   for (var i = 0; i < dates.length; i++) {
-//   //for (var i = 0; i < 12; i++) {
-//     trow = tbody.append("tr");
-//     trow.append("td").text(dates[i]);
-//     trow.append("td").text(openPrices[i]);
-//     trow.append("td").text(highPrices[i]);
-//     trow.append("td").text(lowPrices[i]);
-//     trow.append("td").text(closingPrices[i]);
-//     trow.append("td").text(volume[i]);
-//   }
-// }
-
 function buildPlot_bruce() {
-  //var url = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2017-01-01&end_date=2017-12-31&api_key=${apiKey}`;
-  //var url = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2017-01-01&end_date=2018-11-22&api_key=${apiKey}`;
 
-  d3.csv("jun98tojun20B.csv").then(function(data) {
+  d3.csv("jun98tojun20.csv").then(function(data) {
     //var dates = unpack(data.Dates, 1)
     var dates = []
     var sp500 = []
     var signal = []
-    var cap_growth = []
     // var filtersp500corr = []
   
     data.forEach((d) => {
@@ -340,28 +238,12 @@ function buildPlot_bruce() {
     data.forEach((d) => {
       signal.push(d.signal);
     })
-    data.forEach((d) => {
-      cap_growth.push(d.Capital);
-    })
-    // data.forEach((d) => {
-    //   filtersp500corr.push(d.filterSP500corr);
-    // })new--------------
-  // d3.csv("CGr_model_returns.csv").then(function(data) {
-  //   var cap_growth =[]
     
-  //   data.forEach((c) => {
-  //     cap_growth.push(c.Capital );
-  //   })
-    //end new-----------
-    //console.log(data);
+  
   
     console.log(dates);
     console.log(sp500);
     console.log(signal);
-    console.log(cap_growth);
-    // console.log(filtersp500corr);
-  
-    //console.log(data[i].Date);
   
     var selectorOptions = {
       buttons: [{
@@ -385,6 +267,26 @@ function buildPlot_bruce() {
           count: 1,
           label: '1y'
       }, {
+          step: 'year',
+          stepmode: 'backward',
+          count: 3,
+          label: '3y'
+      }, {
+          step: 'year',
+          stepmode: 'backward',
+          count: 5,
+          label: '5y'
+      }, {
+          step: 'year',
+          stepmode: 'backward',
+          count: 10,
+          label: '10y'
+      }, {
+          step: 'year',
+          stepmode: 'backward',
+          count: 20,
+          label: '20y'
+      }, {
           step: 'all',
       }],
     };
@@ -392,7 +294,7 @@ function buildPlot_bruce() {
     var trace1 = {
       type: "scatter",
       mode: "lines",
-      name: name,
+      name: "SP500",
       x: dates,
       y: sp500,
       line: {
@@ -403,7 +305,7 @@ function buildPlot_bruce() {
     var trace2 = {
       type: "scatter",
       mode: "lines",
-      name: name,
+      name: "Signal",
       x: dates,
       y: signal,
       yaxis: "y2",
@@ -412,32 +314,10 @@ function buildPlot_bruce() {
       }
     };
 
-    var trace3 = {
-      type: "scatter",
-      mode: "lines",
-      name: name,
-      x: dates,
-      y: cap_growth,
-      yaxis: "y3",
-      line: {
-        color: "green"
-      }
-    };
-
-    // Candlestick Trace
-    // var trace2 = {
-    //   type: "candlestick",
-    //   x: dates,
-    //   // high: highPrices,
-    //   // low: lowPrices,
-    //   open: sp500corr,
-    //   close: filtersp500corr
-    // };
-
-    var data = [trace1, trace2, trace3];
+    var data = [trace1, trace2];
 
     var layout = {
-      title: `SP500 closing prices vs Signal`,
+      title: `S&P 500 Closing Prices vs Signal`,
       // autosize: true,
       // width: 500,
       height: 700,
@@ -454,38 +334,28 @@ function buildPlot_bruce() {
       },
       yaxis2: {
         title: 'Signal Std.',
-        titlefont: {color: 'rgb(148, 103, 189)'},
-        tickfont: {color: 'rgb(148, 103, 189)'},
         overlaying: 'y',
         side: 'left'
       },
 
-      yaxis3: {
-        title: 'Capital',
-        titlefont: {color: 'rgb(148, 103, 189)'},
-        tickfont: {color: 'rgb(148, 103, 189)'},
-        overlaying: 'y1',
-        side: 'left'
-      },
-      showlegend: true
+      showlegend: true,
+      legend: {
+        x: 1,
+        xanchor: 'right',
+        y: 1.1
+      }
     };
 
-    // Plotly - Making a Responsive Chart:
-    // https://plotly.com/javascript/configuration-options/#making-a-responsive-chart
     Plotly.newPlot("plot", data, layout, {responsive: true});
 
   });
-}
+};
 
 buildPlot_bruce();
-// getMonthlyData();
-
-
-
 
 
 //##################################################################
-d3.csv("Ben_Final_Data_df.csv").then(function(data) {
+d3.csv("Ben_Final2_Data_df.csv").then(function(data) {
   //var dates = unpack(data.Dates, 1)
   var dates = []
   var sp500 = []
@@ -501,22 +371,16 @@ d3.csv("Ben_Final_Data_df.csv").then(function(data) {
   data.forEach((d) => {
     Z_Score.push(d.Z_Score);
   })
- 
- 
-  //console.log(data);
 
   console.log(dates);
   console.log(sp500);
   console.log(Z_Score);
   
-  //console.log(data[i].Date);
 })
 
 function buildPlot_ben() {
-  //var url = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2017-01-01&end_date=2017-12-31&api_key=${apiKey}`;
-  //var url = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2017-01-01&end_date=2018-11-22&api_key=${apiKey}`;
 
-  d3.csv("Ben_Final_Data_df.csv").then(function(data) {
+  d3.csv("Ben_Final2_Data_df.csv").then(function(data) {
     //var dates = unpack(data.Dates, 1)
     var dates = []
     var sp500 = []
@@ -531,14 +395,10 @@ function buildPlot_ben() {
     data.forEach((d) => {
       Z_Score.push(d.Z_Score);
     })
-    
-    //console.log(data);
   
     console.log(dates);
     console.log(sp500);
     console.log(Z_Score);
-  
-    //console.log(data[i].Date);
   
     var selectorOptions = {
       buttons: [{
@@ -562,6 +422,26 @@ function buildPlot_ben() {
           count: 1,
           label: '1y'
       }, {
+          step: 'year',
+          stepmode: 'backward',
+          count: 3,
+          label: '3y'
+      }, {
+          step: 'year',
+          stepmode: 'backward',
+          count: 5,
+          label: '5y'
+      }, {
+          step: 'year',
+          stepmode: 'backward',
+          count: 10,
+          label: '10y'
+      }, {
+          step: 'year',
+          stepmode: 'backward',
+          count: 20,
+          label: '20y'
+      }, {
           step: 'all',
       }],
     };
@@ -569,7 +449,7 @@ function buildPlot_ben() {
     var trace1 = {
       type: "scatter",
       mode: "lines",
-      name: name,
+      name: "SP500",
       x: dates,
       y: sp500,
       line: {
@@ -580,7 +460,7 @@ function buildPlot_ben() {
     var trace2 = {
       type: "scatter",
       mode: "lines",
-      name: name,
+      name: "Signal",
       x: dates,
       y: Z_Score,
       yaxis: "y2",
@@ -589,11 +469,10 @@ function buildPlot_ben() {
       }
     };
 
-
     var data = [trace1, trace2];
 
     var layout = {
-      title: `SP500 Closing Prices`,
+      title: `S&P 500 vs Correlation Rolling Z-Score`,
       // autosize: true,
       // width: 500,
       height: 700,
@@ -610,21 +489,21 @@ function buildPlot_ben() {
       },
       yaxis2: {
         title: 'Correlation Rolling Z-Score',
-        titlefont: {color: 'rgb(148, 103, 189)'},
-        tickfont: {color: 'rgb(148, 103, 189)'},
         overlaying: 'y',
         side: 'left'
       },
-      showlegend: true
+      showlegend: true,
+      legend: {
+        x: 1,
+        xanchor: 'right',
+        y: 1.1
+      }
     };
 
-    // Plotly - Making a Responsive Chart:
-    // https://plotly.com/javascript/configuration-options/#making-a-responsive-chart
     Plotly.newPlot("plot", data, layout, {responsive: true});
 
   });
 }
 
-buildPlot_ben();
-// //getMonthlyData();
+// buildPlot_ben();
 
